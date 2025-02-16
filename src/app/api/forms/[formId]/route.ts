6811fce4ffd5f83fca9159169280215917c57409
Dhanasaitholeti <dhanasai5566@gmail.com>
2025-02-16 06:18:03 +0530

    added api endpoint for getting individual form

---
+++ src/app/api/forms/[formId]/route.ts
@@ -0,0 +1,45 @@
+import type { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ formId: string }> }
+) {
+  try {
+    const data = await prisma.form.findUnique({
+      where: {
+        id: (await params).formId,
+      },
+      include: {
+        fields: true,
+      },
+    });
+
+    return Response.json({ data });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ formId: string }> }
+) {
+  try {
+    const body = await request.json();
+    const formId = (await params).formId;
+
+    const updatedForm = await prisma.form.update({
+      where: { id: formId },
+      data: body,
+    });
+
+    return Response.json({ data: updatedForm });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
--- src/app/api/forms/route.ts
+++ src/app/api/forms/route.ts
@@ -4,9 +4,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
+  try {
+    const body = await request.json();
 
-  const data = await prisma.form.create({ data: body });
+    console.log(body.fields, "body");
 
-  return Response.json({ body });
+    const data = await prisma.form.create({
+      data: {
+        userId: body.userId,
+        title: body.title,
+        description: body.description,
+        isPublished: body.isPublished,
+        createdAt: body.createdAt,
+        updatedAt: body.updatedAt,
+        fields: {
+          create: body.fields.map((field: any) => ({
+            label: field.label,
+            fieldType: field.fieldType,
+            options: field.options,
+            isRequired: field.isRequired,
+            position: field.position,
+          })),
+        },
+      },
+      include: {
+        fields: true,
+      },
+    });
+
+    return Response.json({ body: data });
+  } catch (error) {
+    console.log(error);
+    return Response.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
